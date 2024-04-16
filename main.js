b30ecd3ee37787c891ea9ac34786b6d30eb9a819
Julia Hokari <106668432+CalssoneJulia@users.noreply.github.com>
2024-04-16 08:27:15 -0300

    julia

--- main.js
+++ main.js
@@ -5,17 +5,18 @@ function verificarTime() {
 
   while (continuarPerguntando) {
     
-    var respostaTime = prompt("Qual o melhor time?");
+    var respostaTime = prompt("Qual filósofo acredita que tudo flui?");
 
     
-    if (respostaTime.toLowerCase() === "athlético pr") {
-      alert("Isso mesmo! O Athlético PR é o melhor time!");
-      continuarPerguntando = false; // Sai do loop após a resposta correta
-    } else {
-      alert("Não é isso! Tente novamente.");
-    }
-  }
+    if (respostaTime.toLowerCase() === "Heráclito de Éfeso") { 
+     alert("Correto! pronto para outra pergunta?");
+    continuarPerguntando = false; // sai do loop após a resposta correta 
+     }  else {
+      alert("Não é isso! Tente novamente.")
+     }
+   }    
 }
+
 
 // Chamada da função para iniciar o processo
 verificarTime();
